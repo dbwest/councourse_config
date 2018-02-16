093a1d66c35945b863987046c925e376dedf14bf
[USERNAME] <[EMAIL_ADDRESS]>
2018-02-15 21:22:57 -0500

    update fly script

--- fly_script.sh
+++ fly_script.sh
@@ -3,3 +3,8 @@ export SERVICE_IP=$(kubectl get svc \
     -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 wget -O fly "http://$SERVICE_IP:8080/api/v1/cli?arch=amd64&platform=linux"
 chmod +x fly
+fly -t local login -u concourse -p $PASSWORD -c http://$SERVICE_IP:8080
+export SERVICE_IP=$(kubectl get svc \
+    --namespace default concourse-web \
+    -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+printf "Concourse URL: [http://$SERVICE_IP:8080]\nUsername: concourse\nPassword: $PASSWORD\n"
